6421bbc7c5b055956607d3c97a62171a78e76d1e
DawoonJang <zzanggu2020@naver.com>
2022-07-18 17:08:04 +0900

    Update: Inference

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Mobile_CenterNet_TTFNet
 Tensorflow2 and Keras implementation of CenterNet and TTFNet
 
+## Inference Examples
+<img width="49%" src="https://user-images.githubusercontent.com/89026839/179469473-ab768561-0964-4ec7-ad0b-ebf0c1033570.png"/> <img width="49%" src="https://user-images.githubusercontent.com/89026839/179469556-f5801692-9c5b-43b8-8837-2645b2b2a152.png"/>
 
 ## Reference
-### Ref
 1. Searching for MobileNetV3 https://arxiv.org/abs/1905.02244
 
 2. MobileDets: Searching for Object Detection Architectures for Mobile Accelerators https://arxiv.org/abs/2004.14525
--- model/Neck/FPN.py
+++ model/Neck/FPN.py
@@ -14,28 +14,36 @@ def FPN(x, config=None):
     }
     
     C2, C3, C4, C5=x
-
+    ##################################################
     P5=_Conv(C5, filters=filters[0], kernel_size=1, strides=1, prefix="C5Residual/", **config_dict)
-
     P5_upsampled=UpSampling2D(name='P5_upsample')(P5)
+
     P4=_Conv(C4, filters=filters[0], kernel_size=1, strides=1, prefix="C4/", **config_dict)
     P4=Add(name='P4_merged')([P5_upsampled, P4])
+    ##################################################
+
+    ##################################################
     if isLite:
         P4=_SeparableConv(P4, filters=filters[1], kernel_size=3, strides=1, prefix="P4/", **config_dict)
     else:
         P4=_Conv(P4, filters=filters[1], kernel_size=3, strides=1, prefix="P4/", **config_dict)
-
     P4_upsampled=UpSampling2D(name='P4_upsample')(P4)
+
     P3=_Conv(C3, filters=filters[1], kernel_size=1, strides=1, prefix="C3/", **config_dict)
     P3=Add(name='P3_merged')([P4_upsampled, P3])
+    ##################################################
+
+    ##################################################
     if isLite:
         P3=_SeparableConv(P3, filters=filters[2], kernel_size=3, strides=1, prefix="P3/", **config_dict)
     else:
         P3=_Conv(P3, filters=filters[2], kernel_size=3, strides=1, prefix="P3/", **config_dict)
-
     P3_upsampled=UpSampling2D(name='P3_upsample')(P3)
+
     P2=_Conv(C2, filters=filters[2], kernel_size=1, strides=1, prefix="C2/", **config_dict)
     P2=Add(name='P2_merged')([P3_upsampled, P2])
+    ##################################################
+    
     if isLite:
         P2=_SeparableConv(P2, filters=filters[3], kernel_size=3, strides=1, prefix="P2/", **config_dict)
     else:
